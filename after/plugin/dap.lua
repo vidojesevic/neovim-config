d3002a3edcc670d4b794c7ed136c5e53cb0b5aff
Vidoje <vidoje.sevic.275.19@ict.edu.rs>
2023-12-24 21:20:48 +0100

    configuration of dap

---
+++ after/plugin/dap.lua
@@ -0,0 +1,21 @@
+local dap = require'dap'
+dap.adapters.php = {
+    type = 'executable',
+    command = 'nodejs',
+    args = {"/opt/vscode-php-debug/out/phpDebug.js"},
+}
+
+dap.configurations.php = {
+    {
+        type = 'php',
+        request = 'launch',
+        name = 'Listen for xdebug',
+        port = '9003',
+        log = true,
+        serverSourceRoot = '/srv/www/',
+        localSourceRoot = '/home/www/VVV/www/',
+    },
+}
+
+require("nvim-dap-virtual-text").setup()
+require("dapui").setup()
