94441e1edb6ac5c0099d4788095c57408fdf453e
vidoje <vidoje.sevic.275.19@ict.edu.rs>
2024-03-04 01:16:49 +0100

    [FEATURE] AI
    - Add codeium assistant

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -106,4 +106,17 @@ return require('packer').startup(function(use)
         'renerocksai/telekasten.nvim',
         requires = {'nvim-telescope/telescope.nvim'}
     }
+
+    -- AI helpers --
+    use {
+        "Exafunction/codeium.nvim",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            require("codeium").setup({
+            })
+        end
+    }
 end)
