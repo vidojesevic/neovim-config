d79f1a4b7625c74f00dd829591bdfe42a1b6cc61
vidoje <vidoje.sevic.275.19@ict.edu.rs>
2024-03-07 09:47:36 +0100

    [Plugin] Harpoon
    - Fix Harpoon width for large filepaths

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,6 +4,11 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
+require("harpoon").setup({
+    menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+    }
+})
 -- vim.keymap.set("n", "<F5>", function() ui.nav_file(1) end)
 -- vim.keymap.set("n", "<F6>", function() ui.nav_file(2) end)
 -- vim.keymap.set("n", "<F7>", function() ui.nav_file(3) end)
