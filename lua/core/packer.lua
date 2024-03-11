93d7a551d68198fc66f0bd22a1dba528ef9ad455
vidoje <vidoje.sevic.275.19@ict.edu.rs>
2024-03-11 20:25:53 +0100

    Update branch

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -117,6 +117,7 @@ return require('packer').startup(function(use)
         config = function()
             require("codeium").setup({
             })
+            -- Repams --
             vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
             vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
             vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
