6044b472f87244ddae082a45d2f9c6e4590b32d5
Vidoje <vidoje.sevic@lobbster.co>
2024-09-04 20:25:21 +0200

    feat: Add whichkey.lua and modify init.lua

--- init.lua
+++ init.lua
@@ -130,6 +130,30 @@ vim.keymap.set("n", "<leader>ls", function()
 	end
 end, { noremap = true, silent = true })
 
+-- -- PHP author docblock on aech .php file --
+-- local function author_docblock()
+-- 	local date = os.date("%m/%d/%y")
+-- 	return string.format(
+-- 		[[
+-- <?php
+-- /**
+--  * @author    Vidoje Šević
+--  * @website   https://www.vsevic.com
+--  * @email     vidoje@vsevic.com
+--  * @date      %s
+--  */
+-- ]],
+-- 		date
+-- 	)
+-- end
+--
+-- vim.api.nvim_create_autocmd("BufNewFile", {
+-- 	pattern = "*.php",
+-- 	callback = function()
+-- 		vim.api.nvim_buf_set_lines(0, 0, 0, false, vim.fn.split(author_docblock(), "\n"))
+-- 	end,
+-- })
+
 require("lazy").setup({
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	{ "numToStr/Comment.nvim", opts = {} },
